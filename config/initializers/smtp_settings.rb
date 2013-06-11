e9f1284d17e5b316a209655887a48eb8924dc7ad
yahtaa <dmull01@gmail.com>
2013-06-11 10:43:48 -0400

    Update SMTP vars

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {
-  :address        => "smtp.sendgrid.net",
-  :port           => "25",
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => ENV['SENDGRID_DOMAIN']
+  :address => "smtp.gmail.com",
+	:port => 587,
+	:authentication => :plain,
+	:domain => ENV['CHATTDRAIN_SMTP_USER'],
+	:user_name => ENV['CHATTDRAIN_SMTP_USER'],
+	:password => ENV['CHATTDRAIN_SMTP_PASSWORD']
 }
